c1ea92df987fc09fc773dd0217af062eac6eb010
Malcolm Beattie <mbeattie@clueful.co.uk>
2025-07-12 19:42:51 +0100

    examples/gpio: add preprocessor defines for GD_ENABLE_GPIO_WRITES (defaulting to 1) and GD_ENABLE_VERBOSE (defaulting to 0). Setting GD_ENABLE_GPIO_WRITES to 0 means all initialisation and writes for GPIO pin *output* is ignores. Enabling GD_ENABLE_VERBOSE does printf immediately before each init or read/write of a set of GPIO pins.

--- examples/gpio/cu/gd_pins.c
+++ examples/gpio/cu/gd_pins.c
@@ -1,27 +1,41 @@
 #include "hardware/gpio.h"
 #include "gd_config.h"
 #include "gd_pins.h"
+#if GD_ENABLE_GPIO_VERBOSE
+#include "stdio.h"
+#endif
 
 void gd_init_out_pins(gpio_dev_t *gd) {
         gd_pins_t *p = &gd->cfg.out_pins;
 
         // Initialise p->count+1 GPIO pins starting at p->base
         // for output
+#if GD_ENABLE_GPIO_VERBOSE
+        printf("init GPIO out %d..%d\n", p->base, p->base + p->count);
+#endif
+#if GD_ENABLE_GPIO_WRITES
         for (uint i = 0; i <= p->count; i++) {
                 uint gpio = p->base + i;
                 gpio_init(gpio);
                 gpio_set_dir(gpio, GPIO_OUT);
         }
+#endif
 }
 
 void gd_write_out_pins(gpio_dev_t *gd, uint8_t val) {
         gd_pins_t *p = &gd->cfg.out_pins;
 
+#if GD_ENABLE_GPIO_VERBOSE
+        printf("GPIO write %d..%d: 0x%02x\n",
+                p->base, p->base + p->count, (unsigned int)val);
+#endif
+#if GD_ENABLE_GPIO_WRITES
         for (uint i = 0; i <= p->count; i++) {
                 bool b = val & 0x01;
                 gpio_put(p->base + i, b);
                 val >>= 1;
         }
+#endif
 }
 
 void gd_init_in_pins(gpio_dev_t *gd) {
@@ -29,6 +43,9 @@ void gd_init_in_pins(gpio_dev_t *gd) {
 
         // Initialise p->count+1 GPIO pins starting at p->base
         // for input
+#if GD_ENABLE_GPIO_VERBOSE
+        printf("init GPIO in %d..%d\n", p->base, p->base + p->count);
+#endif
         for (uint i = 0; i <= p->count; i++) {
                 uint gpio = p->base + i;
                 gpio_init(gpio);
@@ -43,5 +60,9 @@ uint8_t gd_read_in_pins(gpio_dev_t *gd) {
         val >>= p->base; // shift down so p->base pin level is bit 0
         // Mask out bits other than bit number p->count and below
         val &= (1 << (p->count+1)) - 1;
+#if GD_ENABLE_GPIO_VERBOSE
+        printf("GPIO read %d..%d: 0x%02x\n",
+                p->base, p->base + p->count, (unsigned int)val);
+#endif
         return val;
 }
--- examples/gpio/cu/gd_pins.h
+++ examples/gpio/cu/gd_pins.h
@@ -3,6 +3,14 @@
 
 #include "gd_dev.h"
 
+#ifndef GD_ENABLE_GPIO_WRITES
+#define GD_ENABLE_GPIO_WRITES 1
+#endif
+
+#ifndef GD_ENABLE_GPIO_VERBOSE
+#define GD_ENABLE_GPIO_VERBOSE 0
+#endif
+
 void gd_init_out_pins(gpio_dev_t *gd);
 void gd_write_out_pins(gpio_dev_t *gd, uint8_t val);
 void gd_init_in_pins(gpio_dev_t *gd);
