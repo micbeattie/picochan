micbeattie/picochan

commit b64dff369b5775954368c598c68005ed4a7f05ae

dmachan, css, cu: major tidying of channel API and IRQ handling in preparation for adding piochan.

src/picochan/css/css.c
@@ -46,82 +46,81 @@ static void css_try_set_core_num() {
         else if ((uint)CSS.core_num != core_num)
                 panic("CSS configured from multiple cores");
 
-	PCH_CSS_TRACE(PCH_TRC_RT_CSS_CORE_NUM,
+	PCH_CSS_TRACE(PCH_TRC_RT_CSS_SET_CORE_NUM,
                 ((struct pch_trdata_byte){(uint8_t)core_num}));
 }
 
-static void css_irq_set_exclusive_handler(pch_trc_record_type_t rt, irq_num_t irqnum, irq_handler_t handler) {
-        css_try_set_core_num();
-        irq_set_exclusive_handler(irqnum, handler);
-
+static void trace_set_irq_handler(pch_trc_record_type_t rt, irq_num_t irqnum, irq_handler_t handler, int16_t order_priority_opt) {
         PCH_CSS_TRACE(rt, ((struct pch_trdata_irq_handler){
                         .handler = (uint32_t)handler,
-                        .order_priority = -1,
+                        .order_priority = order_priority_opt,
                         .irqnum = (uint8_t)irqnum
                 }));
 }
 
-static void css_irq_add_shared_handler(pch_trc_record_type_t rt, irq_num_t irqnum, irq_handler_t handler, uint8_t order_priority) {
+static void configure_irq_handler(uint irqnum, irq_handler_t handler, int order_priority) {
         css_try_set_core_num();
-        irq_add_shared_handler(irqnum, handler, order_priority);
+        if (order_priority == -1)
+                irq_set_exclusive_handler(irqnum, handler);
+        else
+                irq_add_shared_handler(irqnum, handler, order_priority);
 
-        PCH_CSS_TRACE(rt, ((struct pch_trdata_irq_handler){
-                        .handler = (uint32_t)handler,
-                        .order_priority = (int16_t)order_priority,
-                        .irqnum = (uint8_t)irqnum
-                }));
+        irq_set_enabled(irqnum, true);
+        trace_set_irq_handler(PCH_TRC_RT_CSS_INIT_IRQ_HANDLER,
+                irqnum, handler, order_priority);
 }
 
 int8_t pch_css_get_core_num(void) {
         return CSS.core_num;
 }
 
-// Configuring DMA IRQ handler
+// Configuring CSS IRQ index
 
 pch_irq_index_t pch_css_get_irq_index(void) {
         return CSS.irq_index;
 }
 
 void pch_css_set_irq_index(pch_irq_index_t irq_index) {
-        if (irq_index < 0 || irq_index >= NUM_DMA_IRQS)
-                panic("invalid DMA IRQ index");
+        if (irq_index < 0 || irq_index >= NUM_IRQ_INDEXES)
+                panic("invalid IRQ index");
 
-        irq_num_t irqnum = dma_get_irq_num((uint)irq_index);
-	PCH_CSS_TRACE(PCH_TRC_RT_CSS_SET_DMA_IRQ,
-                ((struct pch_trdata_irqnum_opt){irqnum}));
-
+	PCH_CSS_TRACE(PCH_TRC_RT_CSS_SET_IRQ_INDEX,
+                ((struct pch_trdata_byte){irq_index}));
+        assert(CSS.irq_index == -1 || CSS.irq_index == irq_index);
         CSS.irq_index = irq_index;
 }
 
-void pch_css_configure_irq_index_shared(pch_irq_index_t irq_index, uint8_t order_priority)
- {
-        pch_css_set_irq_index(irq_index);
-        irq_num_t irqnum = dma_get_irq_num(irq_index);
-        css_irq_add_shared_handler(PCH_TRC_RT_CSS_INIT_DMA_IRQ_HANDLER,
-                irqnum, pch_css_dma_irq_handler, order_priority);
-        irq_set_enabled(irqnum, true);
+void pch_css_set_irq_index_if_needed(void) {
+        if (CSS.irq_index == -1)
+                pch_css_set_irq_index(get_core_num());
 }
 
-void pch_css_configure_irq_index_exclusive(pch_irq_index_t irq_index) {
-        pch_css_set_irq_index(irq_index);
-        irq_num_t irqnum = dma_get_irq_num(irq_index);
-        css_irq_set_exclusive_handler(PCH_TRC_RT_CSS_INIT_DMA_IRQ_HANDLER,
-                irqnum, pch_css_dma_irq_handler);
-        irq_set_enabled(irqnum, true);
+// DMA interrupt
+
+static void configure_dma_irq(int order_priority) {
+        assert(!CSS.dma_irq_configured);
+        pch_css_set_irq_index_if_needed();
+        irq_num_t irqnum = dma_get_irq_num(CSS.irq_index);
+        configure_irq_handler(irqnum, pch_css_dma_irq_handler,
+                order_priority);
+        CSS.dma_irq_configured = true;
 }
 
-void pch_css_configure_irq_index_default_shared(uint8_t order_priority) {
-        pch_irq_index_t irq_index = (pch_irq_index_t)get_core_num();
-        pch_css_configure_irq_index_shared(irq_index, order_priority);
+void pch_css_configure_dma_irq_shared(uint8_t order_priority) {
+        configure_dma_irq(order_priority);
 }
 
-void pch_css_configure_irq_index_default_exclusive() {
-        pch_irq_index_t irq_index = (pch_irq_index_t)get_core_num();
-        pch_css_configure_irq_index_exclusive(irq_index);
+void pch_css_configure_dma_irq_exclusive(void) {
+        configure_dma_irq(-1);
 }
 
-void pch_css_auto_configure_irq_index() {
-        pch_css_configure_irq_index_default_shared(PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+void pch_css_configure_dma_irq_shared_default(void) {
+        configure_dma_irq(PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+}
+
+void pch_css_configure_dma_irq_if_needed(void) {
+        if (!CSS.dma_irq_configured)
+                pch_css_configure_dma_irq_shared_default();
 }
 
 // Configuring function IRQ handler
@@ -136,39 +135,58 @@ void pch_css_set_func_irq(irq_num_t irqnum) {
 	CSS.func_irqnum = (int16_t)irqnum;
 }
 
-void pch_css_configure_func_irq_shared(irq_num_t irqnum, uint8_t order_priority) {
-        css_irq_add_shared_handler(PCH_TRC_RT_CSS_INIT_FUNC_IRQ_HANDLER,
-                irqnum, pch_css_func_irq_handler, order_priority);
-        pch_css_set_func_irq(irqnum);
-        irq_set_enabled(irqnum, true);
-}
-
 void pch_css_configure_func_irq_exclusive(irq_num_t irqnum) {
-        css_irq_set_exclusive_handler(PCH_TRC_RT_CSS_INIT_FUNC_IRQ_HANDLER,
-                irqnum, pch_css_func_irq_handler);
         pch_css_set_func_irq(irqnum);
+        configure_irq_handler(irqnum, pch_css_func_irq_handler, -1);
+}
+
+void pch_css_configure_func_irq_shared(irq_num_t irqnum, uint8_t order_priority) {
+        pch_css_set_func_irq(irqnum);
+        configure_irq_handler(irqnum, pch_css_func_irq_handler,
+                order_priority);
         irq_set_enabled(irqnum, true);
 }
 
-void pch_css_configure_func_irq_unused_shared(bool required, uint8_t order_priority) {
-        int irqnum_opt = user_irq_claim_unused(required);
-        if (irqnum_opt != -1) {
-                irq_num_t irqnum = (irq_num_t)irqnum_opt;
-                pch_css_configure_func_irq_shared(irqnum, order_priority);
-        }
+void pch_css_configure_func_irq_shared_default(irq_num_t irqnum) {
+        pch_css_configure_func_irq_shared(irqnum,
+                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
 }
 
-void pch_css_configure_func_irq_unused_exclusive(bool required) {
+bool pch_css_configure_func_irq_unused_exclusive(bool required) {
         int irqnum_opt = user_irq_claim_unused(required);
         if (irqnum_opt != -1) {
                 irq_num_t irqnum = (irq_num_t)irqnum_opt;
                 pch_css_configure_func_irq_exclusive(irqnum);
+                return true;
         }
+
+        return false;
 }
 
-void pch_css_auto_configure_func_irq(bool required) {
-        pch_css_configure_func_irq_unused_shared(required,
-                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+bool pch_css_configure_func_irq_unused_shared(bool required, uint8_t order_priority) {
+        int irqnum_opt = user_irq_claim_unused(required);
+        if (irqnum_opt != -1) {
+                irq_num_t irqnum = (irq_num_t)irqnum_opt;
+                pch_css_configure_func_irq_shared(irqnum, order_priority);
+                return true;
+        }
+
+        return false;
+}
+
+bool pch_css_configure_func_irq_unused_shared_default(bool required) {
+        int irqnum_opt = user_irq_claim_unused(required);
+        if (irqnum_opt != -1) {
+                irq_num_t irqnum = (irq_num_t)irqnum_opt;
+                pch_css_configure_func_irq_shared_default(irqnum);
+                return true;
+        }
+
+        return false;
+}
+
+void pch_css_auto_configure_func_irq(void) {
+        pch_css_configure_func_irq_unused_shared_default(true);
 }
 
 // Configuring I/O IRQ handler
@@ -183,39 +201,57 @@ void pch_css_set_io_irq(irq_num_t irqnum) {
 	CSS.io_irqnum = (int16_t)irqnum;
 }
 
-void pch_css_configure_io_irq_shared(irq_num_t irqnum, uint8_t order_priority) {
-        css_irq_add_shared_handler(PCH_TRC_RT_CSS_INIT_IO_IRQ_HANDLER,
-                irqnum, pch_css_io_irq_handler, order_priority);
-        pch_css_set_io_irq(irqnum);
-        irq_set_enabled(irqnum, true);
-}
-
 void pch_css_configure_io_irq_exclusive(irq_num_t irqnum) {
-        css_irq_set_exclusive_handler(PCH_TRC_RT_CSS_INIT_IO_IRQ_HANDLER,
-                irqnum, pch_css_io_irq_handler);
         pch_css_set_io_irq(irqnum);
-        irq_set_enabled(irqnum, true);
+        configure_irq_handler(irqnum, pch_css_func_irq_handler, -1);
 }
 
-void pch_css_configure_io_irq_unused_shared(bool required, uint8_t order_priority) {
-        int irqnum_opt = user_irq_claim_unused(required);
-        if (irqnum_opt != -1) {
-                irq_num_t irqnum = (irq_num_t)irqnum_opt;
-                pch_css_configure_io_irq_shared(irqnum, order_priority);
-        }
+void pch_css_configure_io_irq_shared(irq_num_t irqnum, uint8_t order_priority) {
+        pch_css_set_io_irq(irqnum);
+        configure_irq_handler(irqnum, pch_css_func_irq_handler,
+                order_priority);
 }
 
-void pch_css_configure_io_irq_unused_exclusive(bool required) {
+void pch_css_configure_io_irq_shared_default(irq_num_t irqnum) {
+        pch_css_configure_io_irq_shared(irqnum,
+                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+}
+
+bool pch_css_configure_io_irq_unused_exclusive(bool required) {
         int irqnum_opt = user_irq_claim_unused(required);
         if (irqnum_opt != -1) {
                 irq_num_t irqnum = (irq_num_t)irqnum_opt;
                 pch_css_configure_io_irq_exclusive(irqnum);
+                return true;
         }
+
+        return false;
 }
 
-void pch_css_auto_configure_io_irq(bool required) {
-        pch_css_configure_io_irq_shared(required,
-                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+bool pch_css_configure_io_irq_unused_shared(bool required, uint8_t order_priority) {
+        int irqnum_opt = user_irq_claim_unused(required);
+        if (irqnum_opt != -1) {
+                irq_num_t irqnum = (irq_num_t)irqnum_opt;
+                pch_css_configure_io_irq_shared(irqnum, order_priority);
+                return true;
+        }
+
+        return false;
+}
+
+bool pch_css_configure_io_irq_unused_shared_default(bool required) {
+        int irqnum_opt = user_irq_claim_unused(required);
+        if (irqnum_opt != -1) {
+                irq_num_t irqnum = (irq_num_t)irqnum_opt;
+                pch_css_configure_io_irq_shared_default(irqnum);
+                return true;
+        }
+
+        return false;
+}
+
+void pch_css_auto_configure_io_irq(void) {
+        pch_css_configure_io_irq_unused_shared_default(true);
 }
 
 io_callback_t pch_css_set_io_callback(io_callback_t io_callback) {
@@ -232,16 +268,15 @@ io_callback_t pch_css_set_io_callback(io_callback_t io_callback) {
 void pch_css_start(io_callback_t io_callback, uint8_t isc_mask) {
         CSS.isc_enable_mask = isc_mask;
 
-        if (CSS.irq_index == -1)
-                pch_css_auto_configure_irq_index();
+        pch_css_set_irq_index_if_needed();
 
         if (CSS.func_irqnum == -1)
-                pch_css_auto_configure_func_irq(true);
+                pch_css_auto_configure_func_irq();
 
         if (io_callback) {
                 pch_css_set_io_callback(io_callback);
                 if (CSS.io_irqnum == -1)
-                        pch_css_auto_configure_io_irq(true);
+                        pch_css_auto_configure_io_irq();
         }
 }
 

examples/blink/blink_memchan/blink_memchan.c
@@ -28,9 +28,6 @@ const pch_chpid_t CHPID = 0;
 
 static pch_cu_t blink_cu = PCH_CU_INIT(1);
 
-pch_irq_index_t css_dmairqix = -1;
-pch_irq_index_t cu_dmairqix = -1;
-
 extern void blink_cu_init(pch_cu_t *cu, pch_unit_addr_t first_ua);
 
 bool core1_ready;
@@ -38,7 +35,6 @@ bool core1_ready;
 static void core1_thread(void) {
         pch_cus_init(); // could do from core 0
         pch_cus_set_trace(BLINK_ENABLE_TRACE); // could do from core 0
-        pch_cus_configure_irq_index_shared_default(cu_dmairqix);
         
         blink_cu_init(&blink_cu, FIRST_UA);
         pch_cu_register(&blink_cu, CUADDR);
@@ -76,15 +72,11 @@ int main(void) {
         light_led_for_three_seconds();
 
         sleep_ms(2000);
-        css_dmairqix = 0;
-        cu_dmairqix = 1;
 
         pch_memchan_init();
 
         pch_css_init();
         pch_css_set_trace(BLINK_ENABLE_TRACE);
-        pch_css_configure_irq_index_shared(css_dmairqix,
-                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
         pch_css_start(NULL, 0); // must set CSS dmairqix before this
         pch_chpid_t chpid = pch_chp_claim_unused(true);
         pch_chp_alloc(chpid, 1); // allocates SID 0

examples/cardkb/cardkb_memchan/cardkb_memchan.c
@@ -37,9 +37,6 @@ const pch_chpid_t CHPID = 0;
 
 static pch_cu_t cardkb_cu = PCH_CU_INIT(NUM_CARDKB_DEVS);
 
-pch_irq_index_t css_dmairqix = 0;
-pch_irq_index_t cu_dmairqix = 1;
-
 extern void cardkb_cu_init(pch_cu_t *cu, pch_unit_addr_t first_ua, uint16_t num_devices);
 extern void cardkb_i2c_init(i2c_inst_t **i2cp, uint8_t *addrp);
 extern void cardkb_dev_init(pch_unit_addr_t ua, i2c_inst_t *i2c, uint8_t i2c_addr);
@@ -49,7 +46,6 @@ bool core1_ready;
 static void core1_thread(void) {
         pch_cus_init();
         pch_cus_set_trace(CARDKB_ENABLE_TRACE);
-        pch_cus_configure_irq_index_shared_default(cu_dmairqix);
 
         cardkb_cu_init(&cardkb_cu, FIRST_UA, NUM_CARDKB_DEVS);
         pch_cu_register(&cardkb_cu, CUADDR);
@@ -118,15 +114,11 @@ int main(void) {
         light_led_for_three_seconds();
 
         puts("Starting...");
-        css_dmairqix = 0;
-        cu_dmairqix = 1;
 
         pch_memchan_init();
 
         pch_css_init();
         pch_css_set_trace(CARDKB_ENABLE_TRACE);
-        pch_css_configure_irq_index_shared(css_dmairqix,
-                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
         pch_css_start(NULL, 0); // must set CSS dmairqix before this
         pch_chpid_t chpid = pch_chp_claim_unused(true);
         pch_chp_alloc(chpid, 1); // Allocates SID 0

examples/gpio/gpio_memchan/gpio_memchan.c
@@ -32,9 +32,6 @@ const pch_chpid_t CHPID = 0;
 
 static pch_cu_t gd_cu = PCH_CU_INIT(NUM_GPIO_DEVS);
 
-pch_irq_index_t css_dmairqix = -1;
-pch_irq_index_t cu_dmairqix = -1;
-
 extern void gd_cu_init(pch_cu_t *cu, pch_unit_addr_t first_ua, uint16_t num_devices);
 
 bool core1_ready;
@@ -42,7 +39,6 @@ bool core1_ready;
 static void core1_thread(void) {
         pch_cus_init();
         pch_cus_set_trace(GD_ENABLE_TRACE);
-        pch_cus_configure_irq_index_shared_default(cu_dmairqix);
 
         gd_cu_init(&gd_cu, FIRST_UA, NUM_GPIO_DEVS); // must call from core 1
         pch_cu_register(&gd_cu, CUADDR);
@@ -100,15 +96,10 @@ int main(void) {
 
         light_led_for_three_seconds();
 
-        css_dmairqix = 0;
-        cu_dmairqix = 1;
-
         pch_memchan_init();
 
         pch_css_init();
         pch_css_set_trace(GD_ENABLE_TRACE);
-        pch_css_configure_irq_index_shared(css_dmairqix,
-                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
         pch_css_start(NULL, 0); // must set CSS dmairqix before this
         pch_chpid_t chpid = pch_chp_claim_unused(true);
         pch_chp_alloc(chpid, 1); // allocates SID 0

examples/mqtt/mqtt_memchan/mqtt_memchan.c
@@ -33,9 +33,6 @@ const pch_chpid_t CHPID = 0;
 
 static pch_cu_t mqtt_cu = PCH_CU_INIT(NUM_MQTT_DEVS);
 
-pch_irq_index_t css_dmairqix = -1;
-pch_irq_index_t cu_dmairqix = -1;
-
 static void light_led_for_three_seconds() {
         status_led_init_with_context(cyw43_arch_async_context());
         status_led_set_state(true);
@@ -69,7 +66,6 @@ static void core1_thread(void) {
         wifi_connect();
         pch_cus_init(); // could do from core 0
         pch_cus_set_trace(MQTT_ENABLE_TRACE); // could do from core 0
-        pch_cus_configure_irq_index_shared_default(cu_dmairqix);
         
         mqtt_cu_init(&mqtt_cu, FIRST_UA, NUM_MQTT_DEVS);
         pch_cu_register(&mqtt_cu, CUADDR);
@@ -113,15 +109,11 @@ int main(void) {
         stdio_init_all();
         sleep_ms(3000);
         printf("started main on core0\n");
-        css_dmairqix = 0;
-        cu_dmairqix = 1;
 
         pch_memchan_init();
 
         pch_css_init();
         pch_css_set_trace(MQTT_ENABLE_TRACE);
-        pch_css_configure_irq_index_shared(css_dmairqix,
-                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
         pch_css_start(NULL, 0); // must set CSS dmairqix before this
         pch_chpid_t chpid = pch_chp_claim_unused(true);
         pch_chp_alloc(chpid, 1); // allocates SID 0

examples/mqtt_full/mqtt_full_memchan/mqtt_full_memchan.c
@@ -53,9 +53,6 @@ const pch_chpid_t CHPID = 0;
 
 static pch_cu_t mqtt_cu = PCH_CU_INIT(NUM_MQTT_DEVS);
 
-pch_irq_index_t css_dmairqix = -1;
-pch_irq_index_t cu_dmairqix = -1;
-
 static void light_led_ms(uint32_t ms) {
         status_led_init_with_context(cyw43_arch_async_context());
         status_led_set_state(true);
@@ -89,7 +86,6 @@ static void core1_thread(void) {
         wifi_connect();
         pch_cus_init(); // could do from core 0
         pch_cus_set_trace(MQTT_ENABLE_TRACE); // could do from core 0
-        pch_cus_configure_irq_index_shared_default(cu_dmairqix);
         
         mqtt_cu_init(&mqtt_cu, FIRST_UA, NUM_MQTT_DEVS);
         pch_cu_register(&mqtt_cu, CUADDR);
@@ -293,15 +289,11 @@ int main(void) {
         stdio_init_all();
         sleep_ms(2000);
         printf("started main on core0\n");
-        css_dmairqix = 0;
-        cu_dmairqix = 1;
 
         pch_memchan_init();
 
         pch_css_init();
         pch_css_set_trace(MQTT_ENABLE_TRACE);
-        pch_css_configure_irq_index_shared(css_dmairqix,
-                PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
         pch_css_start(io_cb, 0); // start with callbacks disabled for all ISCs
         pch_chpid_t chpid = pch_chp_claim_unused(true);
         pch_chp_alloc(chpid, 3); // allocates SIDs 0-2

src/picochan/base/CMakeLists.txt
@@ -9,13 +9,13 @@ target_sources(picochan_base INTERFACE
         ${CMAKE_CURRENT_LIST_DIR}/bsize/bsize.c
         ${CMAKE_CURRENT_LIST_DIR}/dmachan/irq.c
         ${CMAKE_CURRENT_LIST_DIR}/dmachan/memchan.c
-        ${CMAKE_CURRENT_LIST_DIR}/dmachan/rx_channel.c
-        ${CMAKE_CURRENT_LIST_DIR}/dmachan/uart_rx_channel.c
         ${CMAKE_CURRENT_LIST_DIR}/dmachan/mem_rx_channel.c
-        ${CMAKE_CURRENT_LIST_DIR}/dmachan/tx_channel.c
-        ${CMAKE_CURRENT_LIST_DIR}/dmachan/uart_tx_channel.c
         ${CMAKE_CURRENT_LIST_DIR}/dmachan/mem_tx_channel.c
+        ${CMAKE_CURRENT_LIST_DIR}/dmachan/rx_channel.c
+        ${CMAKE_CURRENT_LIST_DIR}/dmachan/tx_channel.c
         ${CMAKE_CURRENT_LIST_DIR}/dmachan/uartchan.c
+        ${CMAKE_CURRENT_LIST_DIR}/dmachan/uart_rx_channel.c
+        ${CMAKE_CURRENT_LIST_DIR}/dmachan/uart_tx_channel.c
         ${CMAKE_CURRENT_LIST_DIR}/proto/payload.c
         ${CMAKE_CURRENT_LIST_DIR}/trc/trace.c
         ${CMAKE_CURRENT_LIST_DIR}/txsm/txsm.c

src/picochan/base/dmachan/dmachan_trace.h
@@ -10,11 +10,24 @@
 #include "picochan/trc_records.h"
 
 #define PCH_DMACHAN_LINK_TRACE(rt, l, data) \
-        PCH_TRC_WRITE(l->bs, l->bs, (rt), (data))
+        PCH_TRC_WRITE((l)->bs, (l)->bs, (rt), (data))
 
 static inline void trace_dmachan(pch_trc_record_type_t rt, dmachan_link_t *l) {
         PCH_DMACHAN_LINK_TRACE(rt, l, ((struct pch_trdata_dmachan){
                 .dmaid = l->dmaid
+        }));
+}
+
+// Values for pch_trdata_dmachan_byte for PCH_TRC_RT_DMACHAN_DST_RESET
+#define DMACHAN_RESET_PROGRESSING       0
+#define DMACHAN_RESET_COMPLETE          1
+#define DMACHAN_RESET_BYPASSED          2
+#define DMACHAN_RESET_INVALID           3
+
+static inline void trace_dmachan_byte(pch_trc_record_type_t rt, dmachan_link_t *l, uint8_t byte) {
+        PCH_DMACHAN_LINK_TRACE(rt, l, ((struct pch_trdata_dmachan_byte){
+                .dmaid = l->dmaid,
+                .byte = byte
         }));
 }
 

commit 072f0e5000aa8a6474493425defc2fbfc56410e0

css: fix configuration of I/O interrupt handler, both explicit and implicit, which broke in commit b64dff36 in the renaming/tweaking of pch_css_configure_io_irq_exclusive() and pch_css_configure_io_irq_shared().

src/picochan/css/css.c
@@ -233,12 +233,12 @@ void pch_css_set_io_irq(irq_num_t irqnum) {
 
 void pch_css_configure_io_irq_exclusive(irq_num_t irqnum) {
         pch_css_set_io_irq(irqnum);
-        configure_irq_handler(irqnum, pch_css_func_irq_handler, -1);
+        configure_irq_handler(irqnum, pch_css_io_irq_handler, -1);
 }
 
 void pch_css_configure_io_irq_shared(irq_num_t irqnum, uint8_t order_priority) {
         pch_css_set_io_irq(irqnum);
-        configure_irq_handler(irqnum, pch_css_func_irq_handler,
+        configure_irq_handler(irqnum, pch_css_io_irq_handler,
                 order_priority);
 }
 